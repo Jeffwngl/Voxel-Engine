f2b047610b590d8bc515583c8a1bb2b8af7bcf84
Jeff Ubuntu <{ID}+{username}@users.noreply.github.com>
2025-09-29 15:47:34 +1000

    Added frame buffer for window resizing and esc key input to close window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "config.h"
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main() {
     // std::cout << "hello" << std::endl;
     
-    // boilerplate
+    // GLFW initialization
     GLFWwindow* window; // pointer to window
 
     if (!glfwInit()) {
@@ -11,25 +14,45 @@ int main() {
         return -1;
     }
 
+    // glfw version and profile control
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     window = glfwCreateWindow(640, 480, "Voxel Engine", NULL, NULL);
     glfwMakeContextCurrent(window);
 
+    // GLAD initialization
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { // find functions for Open GL
         std::cout << "Glad cannot start." << std::endl; 
         glfwTerminate();
         return -1;
     }
 
+    glViewport(0, 0, 640, 480);
+
     glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents(); // clear event queue every frame
+    while (!glfwWindowShouldClose(window)) { // keeps window up
 
-        glClear(GL_COLOR_BUFFER_BIT);
+        glfwPollEvents(); // checks keyboard or mouse events
+
+        glClear(GL_COLOR_BUFFER_BIT); // resets color buffer to defined color
 
         glfwSwapBuffers(window);
     }
 
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+// resizing window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
