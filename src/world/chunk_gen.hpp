e78a8a627998de9c39b200aa557ae1fb90e510e9
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-04-27 17:43:55 +1000

    merged chunk.h file into chunk.hpp file

--- src/world/chunk_gen.hpp
+++ src/world/chunk_gen.hpp
@@ -1,6 +1,28 @@
 #pragma once
 
-#include "chunk.h"
+#include <unordered_map>
+#include <vector>
+#include <glm/glm.hpp>
+#include "../noise/perlin_gen.hpp"
+
+// #include "chunk.h"
+
+/**
+ * @struct Chunk
+ * @brief Represents a single voxel block chunk in the world
+ * 
+ * Each chunk has a 2D coordinate, a vertex buffer for rendering, and OpenGL calls.
+ */
+struct Chunk {
+    glm::vec2 coord;
+    std::vector<Vertex>vertices;
+    unsigned int VBO, VAO;
+    /**
+     * @brief Becomes true after mesh generation and buffer uploads.
+     */
+    bool ready = false;
+};
+
 
 class ChunkManager {
     private:
