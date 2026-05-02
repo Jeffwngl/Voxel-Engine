8c55ca286ecfacf6f25afb25a331a430ed84466a
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-05-02 15:26:03 +1000

    greedy meshing

---
+++ src/noise/deprecated/perlin_gen.hpp
@@ -0,0 +1,28 @@
+/**
+ * THIS VERSION DOES NOT IMPLEMENT GREEDY MESHING, it generates only visible faces per voxel
+ */
+#pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 tex;
+    float texID;
+};
+
+class PerlinGen {
+    public:
+        static std::vector<Vertex> generate(float scale, int chunkX, int chunkZ);
+        static std::vector<Vertex> generateGreedy(float scale, int chunkX, int chunkZ);
+
+    private:
+        static void addTopFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
+        static void addBottomFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
+        static void addRightFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
+        static void addLeftFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
+        static void addFrontFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
+        static void addBackFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
+};
--- src/noise/perlin_gen.hpp
+++ src/noise/perlin_gen.hpp
@@ -13,12 +13,13 @@ struct Vertex {
 class PerlinGen {
     public:
         static std::vector<Vertex> generate(float scale, int chunkX, int chunkZ);
+        static std::vector<Vertex> generateGreedy(float scale, int chunkX, int chunkZ);
 
     private:
-        static void addTopFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
-        static void addBottomFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
-        static void addRightFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
-        static void addLeftFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
-        static void addFrontFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
-        static void addBackFace(std::vector<Vertex>& v, int x, int z, int y, float ID);
+        static void addTopFaceGreedy(std::vector<Vertex>& v, int x, int z, int y, float ID, int width, int depth);
+        static void addBottomFaceGreedy(std::vector<Vertex>& v, int x, int z, int y, float ID, int width, int depth);
+        static void addFrontFaceGreedy(std::vector<Vertex>& v, int x, int z, int y, float ID, int width, int height);
+        static void addBackFaceGreedy(std::vector<Vertex>& v, int x, int z, int y, float ID, int width, int height);
+        static void addRightFaceGreedy(std::vector<Vertex>& v, int x, int z, int y, float ID, int depth, int height);
+        static void addLeftFaceGreedy(std::vector<Vertex>& v, int x, int z, int y, float ID, int depth, int height);
 };
